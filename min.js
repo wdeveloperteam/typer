b7d52b0856282d8842148c583efde62a259213e6
Ashik Ahmed <87943454+wdeveloperteam@users.noreply.github.com>
2021-09-11 02:13:11 +0600

    added typer js in your website

---
+++ min.js
@@ -0,0 +1,12 @@
+var typed = new Typed('.test', {
+    strings: ['ASHIK AHMED',
+     'YOUTUBER',
+     'WEB DESIGNER',
+     'WRODPRESS EXPERT',
+
+    ],
+    typeSpeed: 40,
+    backSpeed: 40,
+    backDelay: 1500,
+    loop: true,
+  });
